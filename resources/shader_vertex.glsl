41ffb4b5d474ac31e16cbae455453efaa04221b5
connsteele <35544211+connsteele@users.noreply.github.com>
2018-05-16 16:28:39 -0700

    First Upload
    Modifications to base code given to me by Christian Echart Cal Poly CSC 474

---
+++ resources/shader_vertex.glsl
@@ -0,0 +1,21 @@
+#version 410 core
+
+layout(location = 0) in vec3 vertPos;
+layout(location = 1) in int vertimat; //changed from unsigned int, that was causing crashes
+
+out vec3 vertex_pos;
+
+uniform mat4 Manim[200];
+uniform mat4 P;
+uniform mat4 V;
+uniform mat4 M;
+
+
+void main() //Skeleton Vert Shader
+{
+	
+	mat4 Ma = Manim[vertimat];
+	vec4 pos = Ma*vec4(vertPos,1.0);
+	gl_Position = P * V * M * pos;
+	
+}
